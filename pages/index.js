578a61c276fde0d0098df981c7cdda61c576c381
quokka-eating-carrots <ovzip7@gmail.com>
2023-06-02 19:07:05 +0900

    Feat: Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,40 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/utils.module.css';
-// import { getSortedPostsData } from '../lib/posts';
+import { getSortedPostsData } from '../lib/posts';
 import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Date from '../components/Date';
 
 // 서버에서 데이터를 작동할 때
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+// 서버에서 api 호출로 데이터를 작동할 때
 // export async function getStaticProps() {
-//   const allPostsData = getSortedPostsData();
+//   const response = await fetch('http://localhost:3000/api/posts');
+//   const json = await response.json();
 //   return {
 //     props: {
-//       allPostsData
+//       allPostsData: json
 //     }
 //   };
 // }
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   // 클라이언트에서 데이터를 작동할 때
-  const [allPostsData, setAllPostsData] = useState([]);
-  useEffect(() => {
-    fetch('/api/posts')
-      .then((res) => res.json())
-      .then((data) => setAllPostsData(data.allPostsData));
-  }, []);
+  // const [allPostsData, setAllPostsData] = useState([]);
+  // useEffect(() => {
+  //   fetch('/api/posts')
+  //     .then((res) => res.json())
+  //     .then((data) => setAllPostsData(data.allPostsData));
+  // }, []);
   return (
     <Layout home>
       <Head>
@@ -40,11 +53,11 @@ export default function Home() {
           {allPostsData &&
             allPostsData.map(({ id, date, title }) => (
               <li className={utilStyles.listItem} key={id}>
-                {title}
+                <Link href={`/posts/${id}`}>{title}</Link>
                 <br />
-                {id}
-                <br />
-                {date}
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
               </li>
             ))}
         </ul>
